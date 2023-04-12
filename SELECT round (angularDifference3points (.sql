b5d286d90931a761ca7d8f1795f7beee49c1af0f
Mariana Duarte <marianaduarte@macbook-pro-2.wit.ulb.ac.be>
2023-04-12 14:41:50 +0200

    csv file link
    Signed-off-by: Mariana Duarte <marianaduarte@macbook-pro-2.wit.ulb.ac.be>

--- SELECT round (angularDifference3points (.sql
+++ SELECT round (angularDifference3points (.sql
@@ -3,3 +3,7 @@ SELECT round (angularDifference3points ( tgeompoint '[Point (1 1) @2000-01-01, P
 SELECT round (angularDifference3points ( tgeompoint '[Point (1 1) @2000-01-01, Point (2 2) @2000-01-02, Point (1 1) @2000-01-03,Point (4 4) @2000-01-04]'), 4);
 
 SELECT round (angularDifference3points ( tgeompoint '[Point (1 1) @2000-01-01, Point (2 2) @2000-01-02, Point (1 1) @2000-01-03,Point (4 4) @2000-01-04, Point (1 1) @2000-01-05]'), 5);§
+
+
+
+https://www.dropbox.com/s/xnq068p8cy4l870/aisdk-2022-11-01.csv?dl=0
