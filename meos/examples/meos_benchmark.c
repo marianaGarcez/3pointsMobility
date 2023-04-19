5aecdd020fb6fd14fc97c27680859088631bd016
Mariana Duarte <marianaduarte@MacBook-Pro-2.local>
2023-04-19 20:53:21 +0200

    speed
    Signed-off-by: Mariana Duarte <marianaduarte@MacBook-Pro-2.local>

--- meos/examples/meos_benchmark.c
+++ meos/examples/meos_benchmark.c
@@ -47,11 +47,6 @@
 /* MEOS */
 #include <meos.h>
 #include <meos_internal.h>
-#include "../include/general/pg_types.h"
-#include "../include/general/tinstant.h"
-#include "../include/general/tsequence.h"
-#include "../include/general/tsequenceset.h"
-#include "../include/general/type_util.h"
 #include <time.h>
 #include "../include/point/tpoint_spatialfuncs.h"
 #include <libpq-fe.h>
