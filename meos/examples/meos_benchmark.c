0bcfc9a29b896dca5894b96d864ab8accb757315
Mariana Duarte <marianaduarte@macbook-pro-2.wit.ulb.ac.be>
2023-04-20 15:53:48 +0200

    speed
    Signed-off-by: Mariana Duarte <marianaduarte@macbook-pro-2.wit.ulb.ac.be>

--- meos/examples/meos_benchmark.c
+++ meos/examples/meos_benchmark.c
@@ -294,8 +294,13 @@ main(int argc, char **argv)
     fscanf(filePorts, "%s\n", text_buffer2);
     printf("%s\n", text_buffer2);
 
-    //scanf(text_buffer2,"%d,%ld,%s,%lf,%lf,%s,%s,%s\n", &(portRead.id), &(portRead.WorldPortIndexNumber), &(portRead.CountryCode), &(portRead.Latitude), &(portRead.Longitude),
-    //  &(portRead.SuppliesFuelOil),&(portRead.DieselOil),&(portRead.Repairs));
+    scanf(text_buffer2,"%d,%ld,%s,%lf,%lf,%s,%s,%s\n", &(portRead.id), &(portRead.WorldPortIndexNumber), &(portRead.CountryCode), &(portRead.Latitude), &(portRead.Longitude),
+      &(portRead.SuppliesFuelOil),&(portRead.DieselOil),&(portRead.Repairs));
+
+    printf("%d,%ld,%s,%lf,%lf,%s,%s,%s\n",portRead.id,portRead.WorldPortIndexNumber,
+     portRead.CountryCode, portRead.Latitude, portRead.Longitude,
+      portRead.SuppliesFuelOil,portRead.DieselOil,portRead.Repairs);
+
 
     no_ports++;
     //printf("\n%d Ports read.\n",no_ports);
