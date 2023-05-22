2b3fcc746e44129672cf6909cedef7baf9f3b407
Mariana Duarte <marianaduarte@MacBook-Pro-2.local>
2023-05-22 17:33:47 +0200

    add sid verification
    Signed-off-by: Mariana Duarte <marianaduarte@MacBook-Pro-2.local>

--- meos/include/meos.h
+++ meos/include/meos.h
@@ -1371,6 +1371,7 @@ extern double temporal_hausdorff_distance(const Temporal *temp1, const Temporal 
 
 /* Analytics functions for temporal types */
 
+Datum datum_transform(Datum value, Datum srid);
 TInstant *tpointinst_transform(const TInstant *inst, int srid);
 Temporal *geo_to_tpoint(const GSERIALIZED *geo);
 Temporal *temporal_simplify_min_dist(const Temporal *temp, double dist);
