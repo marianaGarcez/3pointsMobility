5440edc45bed13f7d30b6cc171391c3d79fe0cfb
Mariana Duarte <marianaduarte@MacBook-Pro-2.local>
2023-05-22 20:11:47 +0200

    assert both are geodetic
    Signed-off-by: Mariana Duarte <marianaduarte@MacBook-Pro-2.local>

--- meos/examples/meos_benchmark.c
+++ meos/examples/meos_benchmark.c
@@ -277,7 +277,7 @@ main(int argc, char **argv)
        /* separate allships that are near the bounding box */
     for (size_t i = 0; i < numships; i++)
     {
-      if (edwithin_tpoint_geo((const Temporal *) allships[0].trip, ports[2].geom, 10))
+      if (etouches_tpoint_geo((const Temporal *) allships[0].trip, ports[2].geom))
       {
         printf("\n Ship is in the bounding box\n");
       }
